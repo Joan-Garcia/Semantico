72c5041892af149356a9454d92287791e28519d8
MiguelLPmez <mig.angel.1916@gmail.com>
2020-11-29 22:53:57 -0600

    Lógica de optimizador de código

---
+++ src/analizadorsemantico/OptimizadorLocal.java
@@ -0,0 +1,46 @@
+/*
+  Optimización local de tipo:
+  1. Instrucciones que se repiten sin haber tenido modificación alguna en uno de
+     sus valores.
+  Del archivo de cuartetos, cuando se encuentra una línea de asignación, 
+  es decir, que ésta inicia directamente con "=", se debe de asignar un contador
+  por cada id de asignación, por ejemplo:
+    
+                              = temp1 - Cuenta 
+
+  se debe tener un contador para "Cuenta", donde, si se repite una enésima vez,
+  en el archivo optimizado de cuartetos debe aparecer sólo la última asignación.
+
+  Entrada:                                      Contadores de asignaciones:
+  + 1356 15 temp1 
+  = temp1 - Cuenta                              <- Cuenta = 1
+  - 75 5 temp2 
+  = temp2 - Valor                               <- Valor = 1
+  * 45 5 temp3 
+  = temp3 - Cuenta                              <- Cuenta++
+  * 121 Cuenta temp4 
+  = temp4 - Num                                 <- Num = 1
+  + Num 12 temp5 
+  = temp5 - Var_1                               <- Var_1 = 1
+  + Num Var temp6 
+  = temp6 - Valor                               <- Valor++
+    
+  Salida:       *Como "Cuenta" y "Valor" tienen asignaciones sin utilizar
+  * 45 5 temp3 
+  = temp3 - Cuenta                              
+  * 121 Cuenta temp4 
+  = temp4 - Num                                 
+  + Num 12 temp5 
+  = temp5 - Var_1                               
+  + Num Var temp6 
+  = temp6 - Valor     
+ */
+package analizadorsemantico;
+
+/**
+ *
+ * @author Miguel
+ */
+public class OptimizadorLocal {
+  
+}
